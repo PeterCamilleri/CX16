408c3bbb9fe88e46ac2361a51a3b7168e0acc06a
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-06-03 12:21:33 -0400

    Started library rework

--- build_all.bat
+++ build_all.bat
@@ -5,7 +5,7 @@ IF NOT EXIST lib  (md lib)
 IF NOT EXIST temp (md temp)
 
 ECHO ===================================
-ECHO Rebuilding Standard CX16 Binaries
+ECHO Rebuilding Standard CX16 Library
 
 ca65 -o temp/sweet_16.o SW16/sweet_16.a65
 
@@ -17,16 +17,16 @@ ECHO The library cx16.lib contains:
 ar65 t lib\cx16.lib
 
 ECHO ===================================
-ECHO Rebuilding Simulator CX16 Binaries
+ECHO Rebuilding Test65 Simulator Library
 
 ca65 -D sw16_sim_support -o temp/swim_16.o SW16/sweet_16.a65
 
-IF EXIST lib\sim16.lib  (rm lib\sim16.lib)
+IF EXIST lib\t65.lib  (rm lib\t65.lib)
 
-ar65 r lib\sim16.lib temp/swim_16.o
+ar65 r lib\t65.lib temp/swim_16.o
 
 ECHO The library sim16.lib contains:
-ar65 t lib\sim16.lib
+ar65 t lib\t65.lib
 ECHO ===================================
 
 del /Q temp\*.*
